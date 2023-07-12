2765091b526906945e390e7165060da2ff58de18
torrentem <vaciocreativo@gmail.com>
2023-07-12 16:54:27 -0300

    Se agrega tarea clase 15

---
+++ Clase15/js/script.js
@@ -0,0 +1,35 @@
+const input = document.querySelector("input"); // esto es el input
+const addBtn = document.getElementById("addElement"); //Esto es el botón de agregar
+const ul = document.querySelector("ul"); //Acá selecciono la UL
+
+addBtn.addEventListener("click", function(){
+    const text = input.value;
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+
+    p.textContent = text;
+    li.classList.add("resultado");
+
+    if (text != '') {
+    li.appendChild(p);
+    li.appendChild(addDeleteBtn());
+    ul.appendChild(li);
+
+    input.value ="";
+
+}} )
+
+
+function addDeleteBtn() {
+    const delBtn = document.createElement("button");
+
+    delBtn.textContent="eliminar";
+    delBtn.classList.add("deletebtn");
+
+    delBtn.addEventListener("click", function(e){
+        const item = e.target.parentElement;
+
+        ul.removeChild(item);
+    });
+    return delBtn;
+}
