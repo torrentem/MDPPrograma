d7dd8835cec97237d7e5856ddd9b046683ea1673
torrentem <vaciocreativo.com>
2023-07-13 16:17:01 -0300

    Correcciones en formato de archivos

--- Clase15/js/script.js
+++ Clase15/js/script.js
@@ -2,7 +2,7 @@ const input = document.querySelector("input"); // esto es el input
 const addBtn = document.getElementById("addElement"); //Esto es el botón de agregar
 const ul = document.querySelector("ul"); //Acá selecciono la UL
 
-addBtn.addEventListener("click", function(){
+addBtn.addEventListener("click", function () {
     const text = input.value;
     const li = document.createElement("li");
     const p = document.createElement("p");
@@ -11,22 +11,23 @@ addBtn.addEventListener("click", function(){
     li.classList.add("resultado");
 
     if (text != '') {
-    li.appendChild(p);
-    li.appendChild(addDeleteBtn());
-    ul.appendChild(li);
+        li.appendChild(p);
+        li.appendChild(addDeleteBtn());
+        ul.appendChild(li);
 
-    input.value ="";
+        input.value = "";
 
-}} )
+    }
+})
 
 
 function addDeleteBtn() {
     const delBtn = document.createElement("button");
 
-    delBtn.textContent="X";
+    delBtn.textContent = "X";
     delBtn.classList.add("deletebtn");
 
-    delBtn.addEventListener("click", function(e){
+    delBtn.addEventListener("click", function (e) {
         const item = e.target.parentElement;
 
         ul.removeChild(item);
