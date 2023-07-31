bf6d65aa58f62e744bc3d36d94e2d26ddec502af
torrentem <vaciocreativo@gmail.com>
2023-07-31 17:53:47 -0300

    app3

---
+++ Javascript/Clase3/app3.js
@@ -0,0 +1,14 @@
+const prompt = require("prompt-sync")({sigint: true});
+
+const persona = [];
+
+()=>{
+    dataPersona = {};
+    
+    dataPersona.name = Carlos;
+    return dataPersona;
+    
+};
+    
+console.log(dataPersona);// persona.push(dataPersona);
+    
